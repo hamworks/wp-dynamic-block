d1a78f7ca0e2b0ffd027e8ce4e6b2c01cd014ee5
Hiroshi Urabe <urabe@ham.works>
2020-09-19 14:40:45 +0900

    update doc

--- tests/block/template.php
+++ tests/block/template.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @var array{class_name: string, content: string} $args;
+ * @var array{class_name: string, content: string, wp_block: WP_Block} $args;
  */
 ?>
 <div class="<?php echo esc_attr( $args['class_name'] ); ?>"><?php echo esc_html( $args['content'] ); ?></div>
